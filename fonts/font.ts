26328448c9dd64589e9a6da571e3528e847b587b
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-10-07 18:54:57 +0500

    fonts

--- fonts/font.ts
+++ fonts/font.ts
@@ -1,3 +1,17 @@
-import {Lato} from "next/font/google"
-import { Cormorant_Garamond } from "next/font/google"
-import { Love_Light } from "next/font/google"
+import { Lato, Cormorant_Garamond, Love_Light } from "next/font/google"
+
+export const lato = Lato({
+     weight: ["100" , "300" , "400" , "700" , "900" ],
+    style: ["normal" , "italic"],
+    subsets: ["latin"]
+})
+export const cormorant = Cormorant_Garamond({
+     weight: ["300" , "400" , "700"],
+    style: ["normal" , "italic"],
+    subsets: ["latin"]
+})
+export const love = Love_Light({
+     weight: "400",
+    subsets: ["latin"]
+})
+
