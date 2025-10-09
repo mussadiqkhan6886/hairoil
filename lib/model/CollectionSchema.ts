29ec6f4a060172cdcf02d7b454d177d0ac54a3ee
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-10-09 22:04:58 +0500

    schema end

--- lib/model/CollectionSchema.ts
+++ lib/model/CollectionSchema.ts
@@ -4,9 +4,12 @@ const CollectionSchema = new Schema({
     title: {type:String, required: true},
     image: {type: String, required: true},
     link: {type: String, required: true},
-    slug: {type: String, required: true}
-})
+    slug: {type: String, required: true},
+    
+},
+{timestamps: true}
+)
 
 const collection = mongoose.models.Collection || mongoose.model("Collection", CollectionSchema)
 
-export const collection
+export default collection
