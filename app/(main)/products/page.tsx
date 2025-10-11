29949ae278a879817295d02974a48f99535ec79f
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-10-12 03:36:26 +0500

    now

--- app/(main)/products/page.tsx
+++ app/(main)/products/page.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const page = async () => {
   try {
     // Fetch products from API route
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products`, {next: {revalidate: 10}});
 
     if (!res.ok) {
       throw new Error("Failed to fetch products");
