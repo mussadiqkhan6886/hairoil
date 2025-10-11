2624f354c9ddd89dd2bc2c970032eb4e8acdd0dc
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-10-11 15:31:21 +0500

    done with testimonials completly

--- components/Review.tsx
+++ components/Review.tsx
@@ -25,7 +25,8 @@ const Review = ({data} : {data: reviewType[]}) => {
         <SwiperSlide key={review._id}>
             <div className="bg-white shadow-lg rounded-2xl p-6 text-left h-full flex flex-col justify-between hover:shadow-xl transition-all duration-300">
             <p className="text-gray-700 italic mb-4">“{review.message}”</p>
-            <h3 className={`${instrumental.className} text-main text-xl sm:text-2xl`}>– {review.name}</h3>
+            <h3 className={`${instrumental.className} text-main capitalize text-xl sm:text-2xl`}>– {review.name}</h3>
+            <p className='text-gray-500 text-sm text-center'>{review.designation}</p>
             </div>
         </SwiperSlide>
         ))}
