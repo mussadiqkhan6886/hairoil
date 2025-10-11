e4869ec922170be1fcc417b8cdc442177e763b26
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-10-12 01:08:29 +0500

    completed

--- components/adminComp/Header.tsx
+++ components/adminComp/Header.tsx
@@ -1,5 +1,9 @@
+'use client';
+
 import { instrumental } from '@/fonts/font'
+import axios from 'axios';
 import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation';
 import { FiHome, FiMessageSquare, FiPackage, FiPlusCircle, FiShoppingBag } from 'react-icons/fi'
 
 const Header = () => {
@@ -10,11 +14,23 @@ const Header = () => {
     { name: 'Add Product', href: '/admin-dashboard/add-product', icon: <FiPlusCircle className="w-4 h-4" /> },
     { name: 'Orders', href: '/admin-dashboard/orders-list', icon: <FiShoppingBag className="w-4 h-4" /> },
   ]
+  const pathname = usePathname()
+  const router = useRouter()
+
+  if(pathname === "/admin-dashboard/login"){
+    return null
+  }
+
+  const logout = async () => {
+    const res = await axios.get('/api/logout')
+    router.push("/admin-dashboard/login")
+  }
 
   return (
     <>
         <header className="flex items-center justify-between bg-white px-6 py-4 shadow-sm border-b">
         <h1 className={`${instrumental.className} text-3xl font-semibold text-main`}>Admin Panel</h1>
+        <div className="flex gap-4">
         <Link
           href="/"
           className="flex items-center gap-2 bg-main text-white px-4 py-2 rounded-lg "
@@ -22,6 +38,8 @@ const Header = () => {
           <FiHome className="w-4 h-4" />
           Go Home
         </Link>
+        <button className='underline cursor-pointer' onClick={logout}>Logout</button>
+        </div>
       </header>
 
       {/* Navigation Menu */}
