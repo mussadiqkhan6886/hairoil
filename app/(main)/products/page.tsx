7402754f3cb9105db72ac71bc44a4d6cc3f82883
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-10-12 03:30:34 +0500

    pr

--- app/(main)/products/page.tsx
+++ app/(main)/products/page.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const page = async () => {
   try {
     // Fetch products from API route
-    const res = await fetch(`/api/products`, { cache: "no-store" });
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products`, { cache: "no-store" });
 
     if (!res.ok) {
       throw new Error("Failed to fetch products");
